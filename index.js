822c7ba11ad0470b75e96091f3a0fcab29165a77
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2023-03-20 19:44:36 -0600

    Get tests to pass

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6;
+const num5 = 20;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(num2, num4, num5);
